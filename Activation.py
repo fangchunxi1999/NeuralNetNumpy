d1e60a5ef964c47e5d748420251efc01eee3f47d
fangchunxi1999 <fangchunxi19990113@gmail.com>
2019-11-15 23:46:03 +0700

    (╯°□°）╯︵ ┻━┻

--- Activation.py
+++ Activation.py
@@ -1,42 +1,47 @@
-class Activation:
-    import numpy as np
+import numpy as np
 
-    def softmax(self, Z):
-        return np.exp(Z) / np.sum(np.exp(Z), axis=1, keepdims=True)
 
-    def sigmoid(self, Z):
-        return 1 / (1 + np.exp(-Z))
+def softmax(Z):
+    return np.exp(Z) / np.sum(np.exp(Z), axis=1, keepdims=True)
 
-    def relu(self, Z):
-        return np.maximum(0, Z)
 
-    def activation(self, Z, TypeOfActivation):
-        if TypeOfActivation == 'softmax':
-            A = softmax(Z)
-        elif TypeOfActivation == 'sigmoid':
-            A = sigmoid(Z)
-        elif TypeOfActivation == 'relu':
-            A = relu(Z)
-        else:
-            raise Exception("Not supported activation type")
+def sigmoid(Z):
+    return 1 / (1 + np.exp(-Z))
 
-        return A
 
-    def softmaxDiff(self, Z, A):
-        pass  # (╯°□°）╯︵ ┻━┻)
+def relu(Z):
+    return np.maximum(0, Z)
 
-    def sigmoidDiff(self, Z, A):
-        return A * (1 - A)
 
-    def reluDiff(self, Z, A):
-        return (Z > 0)
+def activation(Z, TypeOfActivation):
+    if TypeOfActivation == 'softmax':
+        A = softmax(Z)
+    elif TypeOfActivation == 'sigmoid':
+        A = sigmoid(Z)
+    elif TypeOfActivation == 'relu':
+        A = relu(Z)
+    else:
+        raise Exception("Not supported activation type")
+    return A
 
-    def activationDiff(self, Z, A, TypeOfActivation):
-        if TypeOfActivation == 'sigmoid':
-            diff = sigmoidDiff(Z, A)
-        elif TypeOfActivation == 'relu':
-            diff = reluDiff(Z, A)
-        else:
-            raise Exception("Not supported activation type")
 
-        return diff
+def softmaxDiff(Z, A):
+    pass  # (╯°□°）╯︵ ┻━┻)
+
+
+def sigmoidDiff(Z, A):
+    return A * (1 - A)
+
+
+def reluDiff(Z, A):
+    return (Z > 0)
+
+
+def activationDiff(Z, A, TypeOfActivation):
+    if TypeOfActivation == 'sigmoid':
+        diff = sigmoidDiff(Z, A)
+    elif TypeOfActivation == 'relu':
+        diff = reluDiff(Z, A)
+    else:
+        raise Exception("Not supported activation type")
+    return diff
--- Layer.py
+++ Layer.py
@@ -1,39 +1,40 @@
-class Layer:
-    import numpy as np
+import numpy as np
+import Activation
 
-    import Activation
 
-    def flatten(self, inputSize: tuple):
-        outputSize = np.prod(inputSize)
-        return (outputSize, "Flatten")
+def flatten(inputSize: tuple):
+    outputSize = np.prod(inputSize)
+    return (outputSize, "Flatten")
 
-    def flattenCal(self, A0, outputSize):
-        A = np.empty((0, outputSize), int)
-        for item in A0:
-            item = item.reshape(1, -1)
-            A = np.vstack((A, item))
-        return A, A
 
-    def dense(self, activation: str, size: int):
-        outputSize = size
-        return (outputSize, activation, "Dense")
+def flattenCal(A0, outputSize):
+    A = np.empty((0, outputSize), int)
+    for item in A0:
+        item = item.reshape(1, -1)
+        A = np.vstack((A, item))
+    return A, A
 
-    def denseCal(self, A0, weight, bias, activation: str):
-        Z = np.dot(A0, weight) + bias
-        A = Activation.activation(Z, activation)
-        return Z, A
 
-    def layer(self, A0, layerIndex, weightList, biasList, layerList):
-        TypeOfLayer = layerList[layerIndex][-1]
+def dense(activation: str, size: int):
+    outputSize = size
+    return (outputSize, activation, "Dense")
 
-        if TypeOfLayer == "Flatten":
-            outputSize = layerList[layerIndex][0]
-            return self.flattenCal(A0, outputSize)
-        elif TypeOfLayer == "Dense":
-            weight = weightList[layerIndex - 1]
-            bias = biasList[layerIndex - 1]
-            activation = layerList[layerIndex][1]
-            return self.denseCal(A0, weight, bias, activation)
 
-        else:
-            raise Exception("Not supported layer type")
+def denseCal(A0, weight, bias, activation: str):
+    Z = np.dot(A0, weight) + bias
+    A = Activation.activation(Z, activation)
+    return Z, A
+
+
+def layer(A0, layerIndex, weightList, biasList, layerList):
+    TypeOfLayer = layerList[layerIndex][-1]
+    if TypeOfLayer == "Flatten":
+        outputSize = layerList[layerIndex][0]
+        return flattenCal(A0, outputSize)
+    elif TypeOfLayer == "Dense":
+        weight = weightList[layerIndex - 1]
+        bias = biasList[layerIndex - 1]
+        activation = layerList[layerIndex][1]
+        return denseCal(A0, weight, bias, activation)
+    else:
+        raise Exception("Not supported layer type")
