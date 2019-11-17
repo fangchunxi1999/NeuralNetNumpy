0a7ba702c2a145e9170400d0e531497263798fc6
fangchunxi1999 <fangchunxi19990113@gmail.com>
2019-11-17 12:32:51 +0700

    lr decay & typo

--- NeuralNet.py
+++ NeuralNet.py
@@ -67,7 +67,7 @@ class NeuralNet:
             self.weightList.append(weightTmp)
             self.biasList.append(biasTmp)
 
-    def train(self, X: np.ndarray, Y: np.ndarray, epoch: int, leaningRate=0.01, batchSize=32, lossFunction='Entropy'):
+    def train(self, X: np.ndarray, Y: np.ndarray, epoch: int, learningRate=0.01, batchSize=32, lossFunction='Entropy', lrDecay=0.1):
         layerCount = len(self.layerList)
         datasetCount = len(X)
 
@@ -75,6 +75,8 @@ class NeuralNet:
 
         for i in range(epoch):
             print("Iteartion: {}/{} ".format(i + 1, epoch), end='')
+
+            learningRate = learningRate * 1 / (1 + lrDecay * epoch)
 
             loss = 0.0
             indices = np.random.permutation(datasetCount)
@@ -92,9 +94,9 @@ class NeuralNet:
                 weightAdj, biasAdj = self.backward(Y_i, Z, A)
                 for j in range(layerCount - 1):
                     self.weightList[j] = self.weightList[j] + \
-                        (leaningRate / datasetCount) * weightAdj[j]
+                        (learningRate / datasetCount) * weightAdj[j]
                     self.biasList[j] = self.biasList[j] + \
-                        (leaningRate / datasetCount) * biasAdj[j]
+                        (learningRate / datasetCount) * biasAdj[j]
 
             self.errorList.append(loss)
 
